054509ca0ef31a6fc7aaa5846f0b6877118bfda1
Sandra Llanos <sllanos@bancolombia.com.co>
2020-04-23 16:47:38 -0500

    parameters by id

--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tarjetas',
@@ -6,10 +7,15 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./tarjetas.component.css']
 })
 export class TarjetasComponent implements OnInit {
-@Input() items: any[] = [];
-  constructor() { }
+  @Input() items: any[] = [];
+  constructor( private router:Router) { }
 
   ngOnInit() {
+  }
+
+  buscarArtista(item) {
+    const artistId = item.id;
+    this.router.navigate(['/artist', artistId])
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -15,7 +15,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQD-h_bue0WU4dwPQ2ohCjvQhRCn6oTykjUcfGDwNMcQvxi7fwzc-hvXGI7yt5CbU_qkqxTTgy9EosPzlY8'
+      'Authorization': 'Bearer BQB2FXwk7F4uxkYYzE7JR2rwCUAsiFwP3o6J1FF_DCvzJkwKaqrFR4fI-ST4k2ZGOA5MOhzpn9B7xd4DVbA'
     });
 
     return this.http.get(url, { headers });
