421360407f8c73df8202ec8824dda63968dc56b9
Sandra Llanos <sllanos@bancolombia.com.co>
2020-04-23 14:09:37 -0500

    centralize requests to spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,28 +6,37 @@ import { map } from "rxjs/operators";
   providedIn: 'root'
 })
 export class SpotifyService {
-  url = 'https://api.spotify.com/v1/browse/new-releases';
 
   constructor(private http: HttpClient) {
     console.log('Spotify service ready')
   }
-  getNewReleases() {
+
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQBd0jE8mbk1C-xJ2tw4-2gpoUjqx4z1xR6KG7PY5ucdarQnI0lieX9p6Pz8R2VWEClUiQtjuGQy_Unvyvk'
     });
 
-    return this.http.get(this.url, { headers })
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases')
       .pipe(map(data => {
         return data['albums'].items;
       }));
   }
 
   getArtist(termino: string) {
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBd0jE8mbk1C-xJ2tw4-2gpoUjqx4z1xR6KG7PY5ucdarQnI0lieX9p6Pz8R2VWEClUiQtjuGQy_Unvyvk'
-    });
+    //   const headers = new HttpHeaders({
+    //     'Authorization': 'Bearer BQBd0jE8mbk1C-xJ2tw4-2gpoUjqx4z1xR6KG7PY5ucdarQnI0lieX9p6Pz8R2VWEClUiQtjuGQy_Unvyvk'
+    //   });
+    //   return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers })
+    //     .pipe(map((data: any) => data.artists.items))
+    // }
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers })
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
       .pipe(map((data: any) => data.artists.items))
   }
 }
